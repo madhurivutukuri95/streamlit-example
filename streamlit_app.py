9af85be40a48a7fbae1e465f82e7bd6d82402bef
madhurivutukuri95 <90256447+madhurivutukuri95@users.noreply.github.com>
2021-11-15 01:22:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
-streamlit hello
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20))
+)
+
+st.table(df.style.set_precision(2))
